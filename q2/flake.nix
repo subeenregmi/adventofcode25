16f87f2e64be85a5bd1cb24217a0a3c96b66e28e
Subeen Regmi <subeenregmi123@gmail.com>
2025-12-04 21:29:44 +0000

    feat: add flake

---
+++ q2/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "advent of code 2025 flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "aoc25";
+
+        packages = with pkgs; [
+          rustc
+          cargo
+        ];
+
+      };
+    };
+}
